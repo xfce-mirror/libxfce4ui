66fb5bbbe4dc00d5258add18a51fbbddba6624d3
Guido Falsi <mad@madpilot.net>
2021-02-04 09:39:28 +0100

    Conditionally add back ungrab call for FreeBSD.
    Multiple users have reported keys not working properly in XFCE after the removal of this line.
    
    On FreeBSD no console switching pauses or other ill effects have been observed with this code line present.
    
    Please note that the FreeBSD vty console driver (and most of the FreeBSD kernel) is completely different than the linux one, so it is possible the issue does not affect it.

--- libxfce4kbd-private/xfce-shortcuts-grabber.c
+++ libxfce4kbd-private/xfce-shortcuts-grabber.c
@@ -22,6 +22,8 @@
 #include <config.h>
 #endif
 
+#include <sys/param.h>
+
 #include <glib.h>
 #include <glib-object.h>
 
@@ -180,6 +182,9 @@ xfce_shortcuts_grabber_keys_changed (GdkKeymap            *keymap,
 
   TRACE ("Keys changed, regrabbing");
 
+#ifdef __FreeBSD__
+  xfce_shortcuts_grabber_ungrab_all (grabber);
+#endif
   xfce_shortcuts_grabber_grab_all (grabber);
 }
 
