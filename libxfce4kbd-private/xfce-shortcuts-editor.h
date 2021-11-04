39cdf5572b4162cd54263caac1fbec36e87b7931
Sergios - Anestis Kefalidis <sergioskefalidis@gmail.com>
2021-11-04 16:55:08 +0200

    Address feedback.
    - Change copyright
    - Remove frame shadow
    - Fix code style
    - Change delete button to reset button
    - Add translatable string

--- libxfce4kbd-private/xfce-shortcuts-editor.h
+++ libxfce4kbd-private/xfce-shortcuts-editor.h
@@ -1,6 +1,6 @@
 /* vi:set expandtab sw=2 sts=2: */
 /*
- * Copyright (c) 2008 Jannis Pohlmann <jannis@xfce.org>
+ * Copyright (c) 2021 Sergios - Anestis Kefalidis <sergioskefalidis@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
