a7fc0da8b0b78fcf4f988cc7f243ec1e05be2352
Gaël Bonithon <gael@xfce.org>
2023-02-08 16:38:36 +0100

    Make Xfconf mandatory

--- libxfce4ui/libxfce4ui.h
+++ libxfce4ui/libxfce4ui.h
@@ -31,9 +31,9 @@
 #include <libxfce4ui/xfce-spawn.h>
 #include <libxfce4ui/xfce-filename-input.h>
 #include <libxfce4ui/xfce-sm-client.h>
+#include <libxfce4ui/xfce-screensaver.h>
 #include <libxfce4ui/xfce-titled-dialog.h>
 #include <libxfce4ui/libxfce4ui-enum-types.h>
-@xfce-screensaver-header@
 
 #undef _LIBXFCE4UI_INSIDE_LIBXFCE4UI_H
 
