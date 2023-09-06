xfce-mirror/libxfce4ui

commit a0203505201af43a8ccc063e536f1353b8c5daa4

shortcuts-grabber: Filter grabbing by key level
The idea is to look at the "level" value from GdkKeymapKey and see if
there is a mismatch with the set modifiers for a shortcut.

Fixes: xfce4-settings#442

libxfce4kbd-private/xfce-shortcuts-grabber.c
@@ -350,11 +350,12 @@ xfce_shortcuts_grabber_ungrab_all (XfceShortcutsGrabber *grabber)
 
 
 static gboolean
-get_entries_for_keyval (GdkKeymap     *keymap,
-                        gint           group,
-                        guint          keyval,
-                        GdkKeymapKey **keys,
-                        guint         *n_keys)
+get_entries_for_keyval (GdkKeymap        *keymap,
+                        gint              group,
+                        guint             keyval,
+                        GdkModifierType   modifiers,
+                        GdkKeymapKey    **keys,
+                        guint            *n_keys)
 {
   GdkKeymapKey *keys1;
   gint n_keys1;
@@ -401,6 +402,23 @@ get_entries_for_keyval (GdkKeymap     *keymap,
             i++;
           else
             keys1[i] = keys1[--n_keys1];
+      }
+  }
+
+  /* Filter keys by level */
+  {
+    gint i;
+
+    /* Remove keys that do not match the level for the given modifiers */
+    for (i = 0; i < n_keys1;)
+      {
+        if ((keys1[i].level > 0 && modifiers == 0)
+            || (keys1[i].level == 0 && (modifiers & (GDK_SHIFT_MASK | GDK_MOD5_MASK))))
+          {
+            keys1[i] = keys1[--n_keys1];
+          }
+        else
+          i++;
       }
   }
 
@@ -483,7 +501,7 @@ xfce_shortcuts_grabber_regrab_all (XfceShortcutsGrabber *grabber)
 
     if (!map_virtual_modifiers (keymap, key->modifiers, &non_virtual_modifiers))
       continue;
-    if (!get_entries_for_keyval (keymap, group, key->keyval, &keys, &n_keys))
+    if (!get_entries_for_keyval (keymap, group, key->keyval, key->modifiers, &keys, &n_keys))
       continue;
 
     already_grabbed = TRUE;
@@ -624,7 +642,7 @@ xfce_shortcuts_grabber_grab (XfceShortcutsGrabber *grabber, XfceKey *key)
 
   if (!map_virtual_modifiers (keymap, key->modifiers, &non_virtual_modifiers))
     return;
-  if (!get_entries_for_keyval (keymap, group, key->keyval, &keys, &n_keys))
+  if (!get_entries_for_keyval (keymap, group, key->keyval, key->modifiers, &keys, &n_keys))
     return;
 
 #ifdef DEBUG_TRACE

commit b2e1dac24e4393badbf0d9ede344f1096b5d8c82

shortcuts-grabber: Fix filtering by level
a0203505 introduced a regression, see
https://gitlab.xfce.org/xfce/libxfce4ui/-/merge_requests/104#note_79217

It seems that a reliable way to filter by level, taking into account the
modifiers from gtk_accelerator_parse(), is to apply the inverse
transformation to gdk_keymap_get_entries_for_keyval(), i.e.
gdk_keymap_translate_keyboard_state(), and see if the keyval matches.

Fixes: a0203505201af43a8ccc063e536f1353b8c5daa4
Related: xfce4-settings#442

libxfce4kbd-private/xfce-shortcuts-grabber.c
@@ -405,18 +405,22 @@ get_entries_for_keyval (GdkKeymap        *keymap,
 
   /* Filter keys by level */
   {
-    gint i;
+    guint keyval_trans;
+    GdkModifierType modifiers_levelled;
 
     /* Remove keys that do not match the level for the given modifiers */
-    for (i = 0; i < n_keys;)
+    for (gint i = 0; i < n_keys;)
       {
-        if ((keys[i].level > 0 && modifiers == 0)
-            || (keys[i].level == 0 && (modifiers & (GDK_SHIFT_MASK | GDK_MOD5_MASK))))
-          {
-            keys[i] = keys[--n_keys];
-          }
-        else
+        modifiers_levelled = modifiers;
+        if (keys[i].level == 0)
+          modifiers_levelled &= ~(GDK_SHIFT_MASK | GDK_MOD5_MASK);
+
+        gdk_keymap_translate_keyboard_state (keymap, keys[i].keycode, modifiers_levelled,
+                                             keys[i].group, &keyval_trans, NULL, NULL, NULL);
+        if (keyval == keyval_trans)
           i++;
+        else
+          keys[i] = keys[--n_keys];
       }
   }
 