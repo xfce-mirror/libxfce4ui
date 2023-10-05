3acfdc68fc0c66b9e7155f16b45234a82f347b6c
Gaël Bonithon <gael@xfce.org>
2023-10-05 13:07:44 +0200

    Remove also pixbuf

--- libxfce4ui/xfce-titled-dialog.c
+++ libxfce4ui/xfce-titled-dialog.c
@@ -82,7 +82,6 @@ struct _XfceTitledDialogPrivate
   GtkWidget *action_area;
   GtkWidget *subtitle_label;
   GtkWidget *subtitle_separator;
-  GdkPixbuf *pixbuf;
   gchar     *subtitle;
   gboolean   use_header;
 };
@@ -275,13 +274,6 @@ xfce_titled_dialog_finalize (GObject *object)
 
   /* release the subtitle */
   g_free (titled_dialog->priv->subtitle);
-
-  /* release the pixbuf */
-  if (titled_dialog->priv->pixbuf)
-    {
-      g_object_unref (titled_dialog->priv->pixbuf);
-      titled_dialog->priv->pixbuf = NULL;
-    }
 
   (*G_OBJECT_CLASS (xfce_titled_dialog_parent_class)->finalize) (object);
 }
