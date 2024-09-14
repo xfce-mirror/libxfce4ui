xfce-mirror/libxfce4ui

commit af2ca58c25f04b2bf1356d28cce3be7b99a480e0

Add XfceClipboardManager
Share the code currently present in xfce4-settings, so that the same
clipboard manager can be used on X11 in xfce4-clipman-plugin, whether
xfsettingsd is running or not.

Apart from formatting the code to suit libxfce4ui, the only changes made
are :
* Rename GsdClipboardManager into XfceClipboardManager
* Replacement of _start() and _stop() by a failable constructor.
* Copy of xfce_xsettings_get_server_time(), which is used here.
* Use g_list_free_full() whenever possible.

libxfce4ui/xfce-clipboard-manager.h
@@ -1,0 +1,34 @@
+/*
+ * Copyright (c) 2024 Gaël Bonithon <gael@xfce.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA
+ */
+
+#ifndef __XFCE_CLIPBOARD_MANAGER_H__
+#define __XFCE_CLIPBOARD_MANAGER_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define XFCE_TYPE_CLIPBOARD_MANAGER (xfce_clipboard_manager_get_type ())
+G_DECLARE_FINAL_TYPE (XfceClipboardManager, xfce_clipboard_manager, XFCE, CLIPBOARD_MANAGER, GObject)
+
+XfceClipboardManager *xfce_clipboard_manager_new (gboolean replace);
+
+G_END_DECLS
+
+#endif /* __XFCE_CLIPBOARD_MANAGER_H__ */

docs/reference/Makefile.am
@@ -24,7 +24,9 @@ CFILE_GLOB=
 # Header files to ignore when scanning
 IGNORE_HFILES=libxfce4ui-alias.h
 if !ENABLE_X11
-IGNORE_HFILES+=xfce-sm-client.h
+IGNORE_HFILES += \
+	xfce-clipboard-manager.h \
+	xfce-sm-client.h
 endif
 
 # Extra files to add when scanning (relative to $srcdir)

docs/reference/libxfce4ui-docs.xml
@@ -49,6 +49,7 @@
 
   <part id="libxfce4ui-sm">
     <title>Xfce Session Management Support</title>
+    <xi:include href="xml/xfce-clipboard-manager.xml"/>
     <xi:include href="xml/xfce-sm-client.xml"/>
     <xi:include href="xml/xfce-screensaver.xml"/>
   </part>
@@ -59,6 +60,10 @@
   <index id="api-index-deprecated">
     <title>Index of deprecated symbols</title>
     <xi:include href="xml/api-index-deprecated.xml"><xi:fallback /></xi:include>
+  </index>
+  <index id="api-index-4-20">
+    <title>Index of new symbols in 4.20</title>
+    <xi:include href="xml/api-index-4.19.5.xml"><xi:fallback /></xi:include>
   </index>
   <index id="api-index-4-18">
     <title>Index of new symbols in 4.18</title>

libxfce4ui/Makefile.am
@@ -72,9 +72,12 @@ libxfce4ui_2_la_LIBADD = \
 
 if ENABLE_X11
 libxfce4ui_headers += \
+	xfce-clipboard-manager.h \
 	xfce-sm-client.h
 
 libxfce4ui_sources += \
+	xfce-clipboard-manager.c \
+	xfce-clipboard-manager.h \
 	xfce-sm-client.c \
 	xfce-sm-client.h
 

libxfce4ui/libxfce4ui.h.in
@@ -35,6 +35,7 @@
 #include <libxfce4ui/libxfce4ui-enums.h>
 #include <libxfce4ui/libxfce4ui-enum-types.h>
 @BEGIN_X11_HEADERS@
+#include <libxfce4ui/xfce-clipboard-manager.h>
 #include <libxfce4ui/xfce-sm-client.h>
 @END_X11_HEADERS@
 @BEGIN_WAYLAND_HEADERS@

libxfce4ui/libxfce4ui.symbols
@@ -137,6 +137,16 @@ xfce_filename_input_desensitise_widget
 #endif
 #endif
 
+/* xfce-clipboard-manager functions */
+#if IN_HEADER(__XFCE_CLIPBOARD_MANAGER_H__)
+#if IN_SOURCE(__XFCE_CLIPBOARD_MANAGER_C__)
+#ifdef ENABLE_X11
+xfce_clipboard_manager_get_type
+xfce_clipboard_manager_new
+#endif
+#endif
+#endif
+
 /* xfce-sm-client functions */
 #if IN_HEADER(__XFCE_SM_CLIENT_H__)
 #if IN_SOURCE(__XFCE_SM_CLIENT_C__)

commit 1eef7a04d6fc180ca62e87c7bf925defbdbbb76b

clipboard-manager: Add missing include guard
Fixes: af2ca58c25f04b2bf1356d28cce3be7b99a480e0

libxfce4ui/xfce-clipboard-manager.h
@@ -17,6 +17,10 @@
  * MA 02110-1301 USA
  */
 
+#if !defined (_LIBXFCE4UI_INSIDE_LIBXFCE4UI_H) && !defined (LIBXFCE4UI_COMPILATION)
+#error "Only <libxfce4ui/libxfce4ui.h> can be included directly, this file is not part of the public API."
+#endif
+
 #ifndef __XFCE_CLIPBOARD_MANAGER_H__
 #define __XFCE_CLIPBOARD_MANAGER_H__
 