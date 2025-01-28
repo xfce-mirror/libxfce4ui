413932b72b84d2778dd164526b294d78da9696bd
Gaël Bonithon <gael@xfce.org>
2025-01-28 17:52:34 +0100

    build: Use VERSION_FULL intead of VERSION or PACKAGE_VERSION

--- xfce4-about/main.c
+++ xfce4-about/main.c
@@ -19,6 +19,9 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
+#ifdef HAVE_XFCE_REVISION_H
+#include "xfce-revision.h"
+#endif
 
 #ifdef HAVE_STDLIB_H
 #include <stdlib.h>
@@ -532,7 +535,7 @@ main (gint argc,
 
   if (G_UNLIKELY (opt_version))
     {
-      g_print ("%s %s (Xfce %s)\n\n", G_LOG_DOMAIN, PACKAGE_VERSION, xfce_version_string ());
+      g_print ("%s %s (Xfce %s)\n\n", G_LOG_DOMAIN, VERSION_FULL, xfce_version_string ());
       g_print ("%s\n", "Copyright (c) 2008-2024");
       g_print ("\t%s\n\n", _("The Xfce development team. All rights reserved."));
       g_print (_("Please report bugs to <%s>."), PACKAGE_BUGREPORT);
