552ab93797f461f53dfc11f63cad44957b178c5d
DictyosteliumDiscoideum <dictyosteliumdiscoideum@protonmail.com>
2025-03-04 20:56:13 -0700

    Fix linker errors

--- libxfce4ui/xfce-list-view.h
+++ libxfce4ui/xfce-list-view.h
@@ -60,60 +60,77 @@ struct _XfceListView
 };
 
 
-
+G_MODULE_EXPORT
 GtkWidget*      xfce_list_view_new                      (void) G_GNUC_MALLOC;
 
+G_MODULE_EXPORT
 gboolean        xfce_list_view_get_single_click         (const XfceListView *list_view);
 
+G_MODULE_EXPORT
 void            xfce_list_view_set_single_click         (XfceListView       *list_view,
                                                          gboolean            single_click);
 
+G_MODULE_EXPORT
 guint           xfce_list_view_get_single_click_timeout (const XfceListView *list_view);
 
+G_MODULE_EXPORT
 void            xfce_list_view_set_single_click_timeout (XfceListView       *list_view,
                                                          guint               single_click_timeout);
 
+G_MODULE_EXPORT
 GtkTreeViewColumn* xfce_list_view_add_possible_column   (const XfceListView *list_view,
                                                          const gchar        *column_id,
                                                          const gchar        *column_title);
 
+G_MODULE_EXPORT
 GtkTreeViewColumn* xfce_list_view_get_column            (XfceListView       *list_view,
                                                          gchar              *column_id);
 
+G_MODULE_EXPORT
 void            xfce_list_view_set_column_visible       (XfceListView       *list_view,
                                                          const gchar        *column_id,
                                                          const gboolean      visible);
 
+G_MODULE_EXPORT
 gboolean        xfce_list_view_get_column_visible       (const XfceListView *list_view,
                                                          const gchar        *column_id);
 
+G_MODULE_EXPORT
 guint           xfce_list_view_get_column_position      (const XfceListView *list_view,
                                                          const gchar        *column_id);
 
+G_MODULE_EXPORT
 void            xfce_list_view_insert_column_at_position(XfceListView       *list_view,
                                                          gchar              *column_id,
                                                          const guint         position);
 
+G_MODULE_EXPORT
 void            xfce_list_view_render_text              (XfceListView       *list_view,
                                                          const gchar        *column_id,
                                                          gint                column);
 
+G_MODULE_EXPORT
 void            xfce_list_view_render_text_with_func    (XfceListView       *list_view,
                                                          const gchar        *column_id,
                                                          GtkTreeCellDataFunc func);
 
+G_MODULE_EXPORT
 void            xfce_list_view_render_pixbuf_text       (XfceListView       *list_view,
                                                          const gchar        *column_id,
                                                          gint                pixbuf_column,
                                                          gint                text_column);
 
+G_MODULE_EXPORT
 void            xfce_list_view_bind_model               (XfceListView       *list_view,
                                                          GtkTreeModel       *model);
 
+G_MODULE_EXPORT
 GtkTreeModel*   xfce_list_view_get_model                (XfceListView       *list_view);
 
+G_MODULE_EXPORT
 gchar*          xfce_list_view_serialize_state          (XfceListView       *list_view);
 
+G_MODULE_EXPORT
 void            xfce_list_view_set_state_from_string    (XfceListView       *list_view,
                                                          gchar              *state);
 
